0509c5410cb02f831847bc8bf6ee5200a6814704
gotjosh <josue.abreu@gmail.com>
2014-07-16 23:44:02 -0400

    New application structure.

---
+++ config/applications.rb
@@ -0,0 +1,20 @@
+LOTUS_ENV = ENV["LOTUS_ENV"] || "development"
+
+require 'bundler/setup'
+Bundler.require(:default, LOTUS_ENV)
+
+module Lian
+
+	class Application < Lotus::Application
+		configure do
+			layout :application
+
+			load_paths << [
+				'app'
+			]
+
+			routes 'config/routes'
+		end
+	end
+
+end
